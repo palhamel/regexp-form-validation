8c7c47e3ed45e9a20e20152967aca26adc10af63
Pål Hamel <pal.hamel@gmail.com>
2021-08-18 15:36:57 +0200

    add check for email

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ document.getElementById('zip').addEventListener('blur', validateZip)
 document.getElementById('email').addEventListener('blur', validateEmail)
 document.getElementById('phone').addEventListener('blur', validatePhone)
 
+//-----------------------------------
 // Validate Letters a-z or A-Z, x num of characters
 function validateName() {
   // Get the value:
@@ -24,11 +25,12 @@ function validateName() {
   }
 }
 
+//-----------------------------------
 function validateZip() {
   const zip = document.getElementById('zip')
   // Will check for: '12345' och '123 45':
   // const re = /^[0-9]{3,5}(\s[0-9]{2})?$/ // will miss '1234'
-  
+
   const re = /^[0-9]{5}$/ // will ok '12345'
   if (!re.test(zip.value)) {
     zip.classList.add('is-invalid')
@@ -36,13 +38,23 @@ function validateZip() {
     zip.classList.remove('is-invalid')
   }
 }
+//-----------------------------------
+function validateEmail() {
+  const email = document.getElementById('email')
+  // Will search for TLDs within range 2-5:
+  const re = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/ 
 
-function validateEmail() {}
+  if (!re.test(email.value)) {
+    email.classList.add('is-invalid')
+  } else {
+    email.classList.remove('is-invalid')
+  }
+}
 
+//-----------------------------------
 function validatePhone() {}
 
-
-
+// + Check all fields before submit
 
 /* 
 ^               // start of line
