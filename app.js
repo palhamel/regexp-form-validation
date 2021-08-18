7c4cfe3f477ecc31dc3ca5fc9e9f9ffaff124201
Pål Hamel <pal.hamel@gmail.com>
2021-08-18 12:04:23 +0200

    add event listeners

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 console.log('here')
 
+// Event listeners - Form Blur event:
+
+document.getElementById('name').addEventListener('blur', validateName)
+document.getElementById('zip').addEventListener('blur', validateZip)
+document.getElementById('email').addEventListener('blur', validateEmail)
+document.getElementById('phone').addEventListener('blur', validatePhone)
+
+
