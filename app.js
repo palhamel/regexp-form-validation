9eb73b8c73d071b95c5dcc67a1625e3beaf4f115
Pål Hamel <pal.hamel@gmail.com>
2021-08-18 14:56:49 +0200

    add check for zip code

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
 // console.log('here')
 
-
 // const validateName = () => {
 // }
-
 
 // Event listeners - Form Blur event:
 document.getElementById('name').addEventListener('blur', validateName)
 document.getElementById('zip').addEventListener('blur', validateZip)
 document.getElementById('email').addEventListener('blur', validateEmail)
 document.getElementById('phone').addEventListener('blur', validatePhone)
-
 
 // Validate Letters a-z or A-Z, x num of characters
 function validateName() {
@@ -20,31 +17,34 @@ function validateName() {
   const re = /^[^\s]+( [^\s]+)+$/
   // const re = /^[a-öA-Ö]{2,30}$/
   // Evaluate:
-  if(!re.test(name.value)){
-    // then error >> BO5 classname invalid
+  if (!re.test(name.value)) {
     name.classList.add('is-invalid')
   } else {
     name.classList.remove('is-invalid')
-
   }
-  
 }
 
 function validateZip() {
-
+  const zip = document.getElementById('zip')
+  // Will check for: '12345' och '123 45':
+  // const re = /^[0-9]{3,5}(\s[0-9]{2})?$/ // will miss '1234'
+  
+  const re = /^[0-9]{5}$/ // will ok '12345'
+  if (!re.test(zip.value)) {
+    zip.classList.add('is-invalid')
+  } else {
+    zip.classList.remove('is-invalid')
+  }
 }
 
-function validateEmail() {
+function validateEmail() {}
 
-}
+function validatePhone() {}
 
-function validatePhone() {
 
-}
 
 
 /* 
-
 ^               // start of line
 [a-zA-Z]{2,}    // will except a name with at least two characters
 \s              // will look for white space between name and surname
