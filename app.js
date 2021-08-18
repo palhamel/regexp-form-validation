64c5badf8cf2491c0242fe11498a0a17560aeb30
Pål Hamel <pal.hamel@gmail.com>
2021-08-18 14:34:09 +0200

    add reg exp checking first and last name

--- app.js
+++ app.js
@@ -1,10 +1,57 @@
-console.log('here')
+// console.log('here')
+
+
+// const validateName = () => {
+// }
+
 
 // Event listeners - Form Blur event:
-
 document.getElementById('name').addEventListener('blur', validateName)
 document.getElementById('zip').addEventListener('blur', validateZip)
 document.getElementById('email').addEventListener('blur', validateEmail)
 document.getElementById('phone').addEventListener('blur', validatePhone)
 
 
+// Validate Letters a-z or A-Z, x num of characters
+function validateName() {
+  // Get the value:
+  const name = document.getElementById('name')
+  // Reg exp in use:
+  const re = /^[^\s]+( [^\s]+)+$/
+  // const re = /^[a-öA-Ö]{2,30}$/
+  // Evaluate:
+  if(!re.test(name.value)){
+    // then error >> BO5 classname invalid
+    name.classList.add('is-invalid')
+  } else {
+    name.classList.remove('is-invalid')
+
+  }
+  
+}
+
+function validateZip() {
+
+}
+
+function validateEmail() {
+
+}
+
+function validatePhone() {
+
+}
+
+
+/* 
+
+^               // start of line
+[a-zA-Z]{2,}    // will except a name with at least two characters
+\s              // will look for white space between name and surname
+[a-zA-Z]{1,}    // needs at least 1 Character
+\'?-?           // possibility of **'** or **-** for double barreled and hyphenated surnames
+[a-zA-Z]{2,}    // will except a name with at least two characters
+\s?             // possibility of another whitespace
+([a-zA-Z]{1,})? // possibility of a second surname
+
+*/
